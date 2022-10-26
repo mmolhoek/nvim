bfefc9151c06a3410213d01b947b01c76ab6d05f
Mischa Molhoek <mischamolhoek@gmail.com>
2022-10-26 12:33:42 +0200

    more finetuning

--- osx_install_neovim_full.sh
+++ osx_install_neovim_full.sh
@@ -17,22 +17,35 @@ fi
 
 echo installing neovim and all needed packeges via brew
 brew install git neovim stylua pyenv pyenv-virtualenv ninja libtool automake cmake pkg-config gettext curl ripgrep fd gnu-sed shellcheck shfmt zk curl jesseduffield/lazygit/lazygit
+echo "alias ls='lazygit'" >> "$HOME"/.zshrc
+alias ls='lazygit'
 
 #Python
 echo installing python 3.10.7 via pyenv
 pyenv install 3.10.7
 pyenv global 3.10.7
 pip3 install black flake8
+/usr/bin/python3 -m pip install pynvim
 
+#Ruby
+if [ "$(ruby --version)" == "ruby not found" ]; then
+  echo installing ruby
+  curl -sSL https://get.rvm.io | bash -s -- --auto-dotfiles
+  export PATH="$PATH:$HOME/.rvm/bin"
+  rvm install 3.1
+  gem install neovim
+  gem environment
+fi
 
 #nvm/npm
 if [[ -f ~/.nvm/nvm.sh ]] ; then
-  . ~/.nvm/nvm.sh
+  . $HOME/.nvm/nvm.sh
 else
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 fi
 nvm install 16.18.0
 nvm alias default 16.18.0
+nvm use 16.18.0
 npm install -g neovim prettier coc-java-lombok ls_emmet
 
 # get 
@@ -91,6 +104,8 @@ else
 fi
 git config --global core.excludesfile ~/.gitignore
 
-echo ready to go. run nvim and it will auto install a lot of packages the first time. let it install. on errors, quit rerun it until it is quit
-echo also test if :checkhealth reports all is fine. fix if not
+echo ready to go. but to make it work you have to start like this:
+echo nvim ~/.config/nvim/lua/user/plugins.lua  and when open do the write command ':w'
+echo nvim will install all needed plugins. let it finish. then exit and restart with nvim
+echo next: test if :checkhealth reports all is fine. fix those that are complaining.
 echo make sure you have selected a java version with sdk use java 17.0.4-amzn before you start nvim
