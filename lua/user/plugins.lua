f3787879cbcbfc7d7cf747876552b537afc98b9c
Mischa Molhoek <mischa.molhoek@persgroep.net>
2022-11-02 11:41:23 +0100

    rm old line

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,7 +65,6 @@ return packer.startup(function(use)
   use "SmiteshP/nvim-navic"
   use "simrat39/symbols-outline.nvim"
   use "b0o/SchemaStore.nvim"
-  -- use "github/copilot.vim"
   use {
     "zbirenbaum/copilot.lua",
     event = { "VimEnter" },
