59507cbe15c4de01637a86aba1657f66ed83fb4d
Mischa Molhoek <mischa.molhoek@persgroep.net>
2023-08-15 09:57:52 +0200

    temp enable transparent background to see keyboard layout on desktop while learning to use aurora sweep

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -16,3 +16,26 @@ if not status_ok then
   -- vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+-- Set transparent background to see sweep keyboard layout that i set as desktop backround image
+
+-- Function to enable the highlights
+vim.g.enable_transparent = function()
+  vim.cmd "highlight Normal guibg=none"
+  vim.cmd "highlight NonText guibg=none"
+  vim.g.normal_transparency_enabled = true
+  vim.notify "Transparent background enabled"
+end
+-- notify
+
+-- Function to disable the highlights
+vim.g.disable_transparent = function()
+  vim.cmd "highlight Normal guibg=NONE"
+  vim.cmd "highlight NonText guibg=NONE"
+  vim.g.normal_transparency_enabled = false
+  vim.notify "Transparent background disabled"
+end
+-- Initialize the highlights
+vim.g.enable_transparent()
+-- Define a command to toggle the highlights
+vim.cmd "command! ToggleTransparency lua if vim.g.normal_transparency_enabled then vim.g.disable_transparent() else vim.g.enable_transparent() end"
